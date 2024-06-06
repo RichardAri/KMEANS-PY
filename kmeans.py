994ef646d03af050b48e0af2e76222c9b6cb5263
RichardAri <richardaripari@gmail.com>
2024-06-06 08:38:57 -0500

    Se añadio codigo para leer el csv

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances_argmin_min
 
+%matplotlib inline
+
+from mpl_toolkits.mplot3d import Axes3D
+plt.rcParams['figure.figsize'] = (16,9)
+plt.style.use('ggplot')
 
 #import de el dataset 
+from google.colab import drive
+drive.mount('/content/drive', force_remount=True)
+df = pd.read_csv('/content/drive/My Drive/SI_L09_KMEANS_DATASET.csv')
+
+df.head()
+
 
 
 
